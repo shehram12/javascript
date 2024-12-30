3b7e08669c2675f19b8c250932f3b6e7caf60c31
shehram12 <161745609+shehram12@users.noreply.github.com>
2024-12-30 20:11:20 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,59 @@
+//const heading = document.getElementById('haeding');
+//console.log(heading);
+
+//const heading =document.getElementsByTagName('h1');
+//console.log(heading);
+
+//const heading = document.querySelectorAll('.heading');
+//console.log(heading);
+
+//const heading = document.querySelector('.heading');
+//const parent= heading.parentNode;
+//console.log(parent);
+
+//const parent =document.querySelector('.parent');
+//console.log(parent.childNodes);
+
+ //const heading =document.querySelector('.heading');
+
+ //console.log(heading.nextElementSibling);
+
+ 
+ //const subheading=document.querySelector('h3');
+
+ //console.log(subheading.previousElementSibling);
+
+ /*const heading = document.querySelector('.heading');
+
+   heading.innerHTML='Web dev is used for adding functionality';
+
+   heading.style.color='green';
+   heading.style.fontSize='100px';
+
+   heading.classList.add('title');
+   heading.classList.remove('title');
+*/
+
+/*const heading = document.createElement('h1');
+heading.innerHTML='js is awesome';
+heading.classList.add('title');
+const parent=document.querySelector('.parent');
+parent.appendChild(heading);
+
+console.log(heading);
+
+
+const subheading = document.createElement('h3');
+subheading.innerHTML='js is awesome';
+heading.insertAdjacentElement('beforebegin',subheading);
+
+console.log(heading);*/
+
+/*const button = document.querySelector('#btn');
+const heading = document.querySelector('#heading');
+
+button.addEventListener('click',function(event){
+    heading.style.color='green';
+    heading.style.fontSize='100px';
+    console.log('button clicked',event);
+})*/
